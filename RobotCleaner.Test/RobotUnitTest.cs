4bd65b48c38ba3b2c62935124d71c441d696d1c0
Jacob Hagstedt <jacob.hagstedt@lamneasoftware.se>
2019-12-12 12:10:20 +0100

    Add test to check so we are not walking outside of grid

--- RobotCleaner.Test/RobotUnitTest.cs
+++ RobotCleaner.Test/RobotUnitTest.cs
@@ -125,7 +125,43 @@ namespace RobotCleaner.Test
                 robot.Execute(direction, rnd.Next(0, 100000));
             }
 
-            Assert.Equal(3467602, robot.GetUniquePositions());
+            Assert.Equal(2584093, robot.GetUniquePositions());
+            Assert.Equal(finalLocation, robot.CurrentLocation);
+        }
+
+        [Fact]
+        public void LongSteps()
+        {
+            Robot robot = new Robot(10, 22);
+            Tuple<int, int> finalLocation = new Tuple<int, int>(90978, -1785);
+
+            Random rnd = new Random(1337);
+            string[] directions = new string[] { "E", "W", "S", "N" };
+
+            for (int i = 0; i < 10; ++i)
+            {
+                string direction = directions.OrderBy(x => rnd.Next()).Take(1).First();
+                robot.Execute(direction, rnd.Next(90000, 100000));
+            }
+
+            Assert.Equal(484730, robot.GetUniquePositions());
+            Assert.Equal(finalLocation, robot.CurrentLocation);
+        }
+
+        [Fact]
+        public void GoFromEdgeToEdge()
+        {
+            Robot robot = new Robot(-100000, -100000);
+            Tuple<int, int> finalLocation = new Tuple<int, int>(100000, 100000);
+
+            robot.Execute("E", 100000);
+            robot.Execute("E", 100000);
+            robot.Execute("E", 100);
+            robot.Execute("S", 100000);
+            robot.Execute("S", 100000);
+            robot.Execute("S", 100);
+
+            Assert.Equal(400001, robot.GetUniquePositions());
             Assert.Equal(finalLocation, robot.CurrentLocation);
         }
     }
